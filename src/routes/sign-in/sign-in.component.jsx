c69caeafcd29c4a5cb160b3de0f560472971a96f
Pabl0Parra <pabsblue16@gmail.com>
2022-07-30 23:08:45 +0200

    finished routing & started setting up firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -3,18 +3,18 @@
 //   createUserProfileDocument,
 // } from "../../utils/firebase/firebase.utils";
 
-// const SignIn = () => {
-//   const logGoogleUser = async () => {
-//     const response = await signInWithGooglePopup();
-//     createUserProfileDocument(response);
-//   };
+const SignIn = () => {
+  //   const logGoogleUser = async () => {
+  //     const response = await signInWithGooglePopup();
+  //     createUserProfileDocument(response);
+  //   };
 
-//   return (
-//     <div>
-//       <h1>Sign In Page</h1>
-//       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      {/* <button onClick={logGoogleUser}>Sign in with Google Popup</button> */}
+    </div>
+  );
+};
 
-// export default SignIn;
+export default SignIn;
