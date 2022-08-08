455c354e4fe3ed5cd61f4d411d13a8aad7292eb3
Pabl0Parra <pabsblue16@gmail.com>
2022-08-08 22:51:31 +0100

    console.log what triggers useSelector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,12 @@
 //* with the selector, i can transform the reducer business logic into readable data
 
-const selectCategories = (state) =>
-  state.categories.categories.reduce((acc, category) => {
+const selectCategories = (state) => {
+  console.log("selector fire");
+
+  return state.categories.categories.reduce((acc, category) => {
     const { title, items } = category; // no need to put .data since category is already the data
     acc[title.toLowerCase()] = items;
     return acc;
   }, {}); //
-
+};
 export default selectCategories;
