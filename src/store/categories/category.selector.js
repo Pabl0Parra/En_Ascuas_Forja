cc5e558bdf6cd4a3ade4e2727ad2b01cc000f330
Pabl0Parra <pabsblue16@gmail.com>
2022-08-09 08:42:38 +0100

    added reselect library & set up category selector to only run when it´s intended to

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,12 +1,21 @@
+//* createSelector does the memoization for us, meaning that if something has already being run, we don´t have to run it again , since we already have that value
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+export const selectCategories = createSelector(
+  // inputs
+  [selectCategoryReducer],
+  // outputs. It will only run if categoriesSlice that we get from selectCategoryReducer has changed
+  (categoriesSlice) => categoriesSlice.categories
+);
+
 //* with the selector, i can transform the reducer business logic into readable data
-
-const selectCategories = (state) => {
-  console.log("selector fire");
-
-  return state.categories.categories.reduce((acc, category) => {
+const selectCategoriesMap = createSelector([selectCategories], (categories) =>
+  categories.reduce((acc, category) => {
     const { title, items } = category; // no need to put .data since category is already the data
     acc[title.toLowerCase()] = items;
     return acc;
-  }, {}); //
-};
-export default selectCategories;
+  }, {})
+);
+
+export default selectCategoriesMap;
