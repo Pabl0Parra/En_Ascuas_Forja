1eec9b425b0d124858ea9932a4865416f6650f23
Pabl0Parra <pabsblue16@gmail.com>
2022-08-09 18:08:32 +0100

    moved all async fetching to one place which redux thunk takes care of & added spinner while loading

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,21 +1,28 @@
-//* createSelector does the memoization for us, meaning that if something has already being run, we don´t have to run it again , since we already have that value
 import { createSelector } from "reselect";
 
 const selectCategoryReducer = (state) => state.categories;
+
 export const selectCategories = createSelector(
-  // inputs
   [selectCategoryReducer],
-  // outputs. It will only run if categoriesSlice that we get from selectCategoryReducer has changed
   (categoriesSlice) => categoriesSlice.categories
 );
 
-//* with the selector, i can transform the reducer business logic into readable data
-const selectCategoriesMap = createSelector([selectCategories], (categories) =>
-  categories.reduce((acc, category) => {
-    const { title, items } = category; // no need to put .data since category is already the data
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {})
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
 );
 
-export default selectCategoriesMap;
+export const selectIsLoading = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.isLoading
+);
+
+export const selectCategoriesIsLoading = createSelector(
+  [selectCategories],
+  (categoriesSlice) => categoriesSlice.IsLoading
+);
--- src/store/store.js
+++ src/store/store.js
@@ -1,33 +1,31 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
-// this will connect to our localStorage
 import storage from "redux-persist/lib/storage";
-//! remove logger before production!
 import logger from "redux-logger";
+import thunk from "redux-thunk";
 
 import { rootReducer } from "./root-reducer";
 
-const persistConfig = {
-  key: "root",
-  storage,
-  blacklist: ["user"],
-};
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// filtering out what`s false --> boolean. We do this in dev to make sure middleware is always true (does not work on production)
-const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
-  Boolean
-);
-
-// if we r not in prod & there is a window object & dev_tools exist, use this enhancer, || otherwise, use the one from redux
-const composedEnhancer =
+const composeEnhancer =
   (process.env.NODE_ENV !== "production" &&
     window &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
 
-const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
