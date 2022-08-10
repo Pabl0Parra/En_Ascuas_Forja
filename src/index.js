dc1336d88a2d939c74d7614b7e7ab5847668ce57
Pabl0Parra <pabsblue16@gmail.com>
2022-08-10 21:57:36 +0100

    added stripe dependencies & set up basic code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
-
 import { store, persistor } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import FooterContainer from "./components/footer-container/footer-container.component";
 import "./index.scss";
@@ -18,7 +19,10 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          {/* any component inside Elements will now have access to stripe */}
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
           <FooterContainer />
         </BrowserRouter>
       </PersistGate>
