74987cf5158debb476b0964fb8081af78a14e73b
Pabl0Parra <pabsblue16@gmail.com>
2022-08-10 12:23:50 +0100

    migrated sign_in from redux-thunk to redux-saga

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -96,7 +96,7 @@ export const createUserDocumentFromAuth = async (
     }
   }
 
-  return userDocRef;
+  return userSnapshot;
 };
 
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
@@ -116,3 +116,16 @@ export const signOutUser = () => signOut(auth);
 // this open listener sees every auth change & then calls the callback function to do something...
 export const onAuthStateChangedListener = (callback) =>
   onAuthStateChanged(auth, callback);
+
+export const getCurrentUser = () => {
+  return new Promise((resolve, reject) => {
+    const unsubscribe = onAuthStateChanged(
+      auth,
+      (userAuth) => {
+        unsubscribe();
+        resolve(userAuth);
+      },
+      reject
+    );
+  });
+};
